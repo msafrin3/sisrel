c04cbbdb2c50f1657f068e3dc192dd532d3267a0
Safrin <admin@Matts-MacBook-Pro.local>
2021-05-24 20:04:12 +0800

    something

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>SISREL | Login</title>
+        <title>SisReL | Login</title>
         <!-- Tell the browser to be responsive to screen width -->
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
         <!-- Bootstrap 3.3.7 -->
@@ -32,7 +32,7 @@
         <div class="login-box">
             <div class="login-logo">
                 <img src="{{ url('') }}/images/logo.png" alt="" style="width:100px;"><br/>
-                <a href="{{ url('') }}"><b>Sis</b>Rel</a>
+                <a href="{{ url('') }}"><b>Sis</b>ReL</a>
             </div>
             <!-- /.login-logo -->
             <div class="login-box-body">
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>SisRel | @yield('title')</title>
+        <title>SisReL | @yield('title')</title>
         <!-- Tell the browser to be responsive to screen width -->
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
         <!-- Bootstrap 3.3.7 -->
@@ -51,11 +51,11 @@
             <!-- Logo -->
             <a href="{{ url('') }}" class="logo">
                 <!-- mini logo for sidebar mini 50x50 pixels -->
-                <span class="logo-mini"><b>S</b>R</span>
+                <span class="logo-mini"><b>S</b>RL</span>
                 <!-- logo for regular state and mobile devices -->
                 <span class="logo-lg">
                     <img src="{{ url('') }}/images/logo.png" alt="">
-                    <b>Sis</b>Rel
+                    <b>Sis</b>ReL
                 </span>
             </a>
             <!-- Header Navbar: style can be found in header.less -->
